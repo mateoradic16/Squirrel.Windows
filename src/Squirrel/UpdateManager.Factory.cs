f81ea5ded7b4cb64836144af928faf5fac906f4d
Mateo Radic <mateoradic16@gmail.com>
2022-08-30 00:05:47 +0200

    Added Nuts

--- src/Squirrel/UpdateManager.Factory.cs
+++ src/Squirrel/UpdateManager.Factory.cs
@@ -54,7 +54,8 @@ namespace Squirrel
             var repoUri = new Uri(repoUrl);
             var userAgent = new ProductInfoHeaderValue("Squirrel", Assembly.GetExecutingAssembly().GetName().Version.ToString());
 
-            if (repoUri.Segments.Length != 3) {
+            if (repoUri.Segments.Length != 3)
+            {
                 throw new Exception("Repo URL must be to the root URL of the repo e.g. https://github.com/myuser/myrepo");
             }
 
@@ -62,12 +63,15 @@ namespace Squirrel
                 .Append(repoUri.AbsolutePath)
                 .Append("/releases");
 
-            
+
             Uri baseAddress;
 
-            if (repoUri.Host.EndsWith("github.com", StringComparison.OrdinalIgnoreCase)) {
+            if (repoUri.Host.EndsWith("github.com", StringComparison.OrdinalIgnoreCase))
+            {
                 baseAddress = new Uri("https://api.github.com/");
-            } else {
+            }
+            else
+            {
                 // if it's not github.com, it's probably an Enterprise server
                 // now the problem with Enterprise is that the API doesn't come prefixed
                 // it comes suffixed
@@ -78,7 +82,8 @@ namespace Squirrel
 
             // above ^^ notice the end slashes for the baseAddress, explained here: http://stackoverflow.com/a/23438417/162694
 
-            using (var client = new HttpClient() { BaseAddress = baseAddress }) {
+            using (var client = new HttpClient() { BaseAddress = baseAddress })
+            {
 
                 client.DefaultRequestHeaders.UserAgent.Add(userAgent);
                 Console.WriteLine(accessToken);
@@ -88,7 +93,75 @@ namespace Squirrel
                 Console.WriteLine(response.ToString());
                 response.EnsureSuccessStatusCode();
                 var releases = SimpleJson.DeserializeObject<List<Release>>(await response.Content.ReadAsStringAsync());
-                if(releases.Count > 0)
+                if (releases.Count > 0)
+                {
+                    var latestRelease = releases
+                        .Where(x => prerelease || !x.Prerelease)
+                        .OrderByDescending(x => x.PublishedAt)
+                        .First();
+                    Console.WriteLine(latestRelease.HtmlUrl);
+                    Console.WriteLine(Json.SimpleJson.SerializeObject(latestRelease.Assets));
+                    var latestReleaseUrl = latestRelease.Url;
+
+                    return new UpdateManager(latestReleaseUrl, applicationName, rootDirectory, urlDownloader, latestRelease, accessToken);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        public static async Task<UpdateManager> NutsUpdateManager(
+            string url,
+            string applicationName = null,
+            string rootDirectory = null,
+            IFileDownloader urlDownloader = null,
+            bool prerelease = false,
+            string accessToken = null)
+        {
+            var repoUri = new Uri(url);
+            var userAgent = new ProductInfoHeaderValue("Squirrel", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+            if (repoUri.Segments.Length != 1)
+            {
+                throw new Exception("Nuts URL must be to the root URL of host e.g. https://updates.mydomain.com/");
+            }
+
+            var releasesApiBuilder = new StringBuilder("repos")
+                .Append(repoUri.AbsolutePath)
+                .Append("/releases");
+
+
+            Uri baseAddress;
+
+            if (repoUri.Host.EndsWith("github.com", StringComparison.OrdinalIgnoreCase))
+            {
+                baseAddress = new Uri("https://api.github.com/");
+            }
+            else
+            {
+                // if it's not github.com, it's probably an Enterprise server
+                // now the problem with Enterprise is that the API doesn't come prefixed
+                // it comes suffixed
+                // so the API path of http://internal.github.server.local API location is
+                // http://interal.github.server.local/api/v3. 
+                baseAddress = new Uri(string.Format("{0}{1}{2}/api/v3/", repoUri.Scheme, Uri.SchemeDelimiter, repoUri.Host));
+            }
+
+            // above ^^ notice the end slashes for the baseAddress, explained here: http://stackoverflow.com/a/23438417/162694
+
+            using (var client = new HttpClient() { BaseAddress = baseAddress })
+            {
+
+                client.DefaultRequestHeaders.UserAgent.Add(userAgent);
+                Console.WriteLine(accessToken);
+                if (!string.IsNullOrWhiteSpace(accessToken))
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", accessToken);
+                var response = await client.GetAsync(releasesApiBuilder.ToString());
+                Console.WriteLine(response.ToString());
+                response.EnsureSuccessStatusCode();
+                var releases = SimpleJson.DeserializeObject<List<Release>>(await response.Content.ReadAsStringAsync());
+                if (releases.Count > 0)
                 {
                     var latestRelease = releases
                         .Where(x => prerelease || !x.Prerelease)
